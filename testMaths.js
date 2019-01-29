8bb7be289046cbcc514f9ea6e0555d68e0da9e4b
anandprs <anandprs@ANANDPRS-LAP.oradev.oraclecorp.com>
2019-01-29 14:10:18 +0530

    commit

--- testMaths.js
+++ testMaths.js
@@ -2,3 +2,18 @@ var mymath = require('./maths');
 
 mymath.add(5,6);
 mymath.prod(6,6);
+
+var exp = require('express');
+
+var app = exp();
+
+app.get('/', function(req,res){
+	res.send("Hello nodeJS webserver");
+});
+
+var port = process.env.PORT || 3000; // PORT for heroku
+
+app.listen(port,function(){
+
+	console.log("Running on configurable port from package.json " + port);
+});
