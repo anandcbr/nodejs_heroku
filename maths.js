f84993630de1336df036266cec9cf8e4333906ab
anandprs <anandprs@ANANDPRS-LAP.oradev.oraclecorp.com>
2019-01-29 13:35:44 +0530

    my first commit

---
+++ maths.js
@@ -0,0 +1,8 @@
+exports.add = function (x,y){
+	console.log("Adding " + x + " and " + y + " , sum is " + (x+y));
+}
+
+exports.prod = function (x,y){
+	console.log("Multiplying " + x + " and " + y + " , product is " + (x*y));
+}
+
---
+++ testMaths.js
@@ -0,0 +1,4 @@
+var mymath = require('./maths');
+
+mymath.add(5,6);
+mymath.prod(6,6);
